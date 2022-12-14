4ec64ac2a7d7ac648bf244cc3edd3b46fd00bbfc
Ingo Hermsen <ingohermsen@gmail.com>
2022-12-14 20:02:54 +0100

    included 'smallest Backend'

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+setURL('http://developerakademie.com/smallest_backend_ever');
+
+function addTask() {
+
+};
+
+function showDropdown() {
+
+};
+
+function openDatePicker() {
+
+};
+
+function commitSubtasks() {
+
+};
+
+function clearInputFields() {
+
+};
