6ae7f0387167d119b50acc36651ed5fcf9561964
Ingo Hermsen <ingohermsen@gmail.com>
2022-12-15 17:01:36 +0100

    restructured dropdowns in add Task

--- script.js
+++ script.js
@@ -2,11 +2,28 @@
 
 setURL('https://gruppe-406.developerakademie.net/smallest_backend_ever');
 
+let categories = [
+    {
+    'name': 'sales',
+    'color':  '#D95030',
+    },
+    {
+    'name': 'backoffice',
+    'color':  ' #497E76',
+    },
+    {
+    'name': 'accounting',
+    'color':  '#FFFF00',
+    },
+];
+
+// add Task form:
+
 function addTask() {
 
 };
 
-function showDropdown() {
+function toggleDropdown() {
 
 };
 
@@ -21,3 +38,5 @@ function commitSubtasks() {
 function clearInputFields() {
 
 };
+
+// 
