c33a7f470004971036888ce89467e9453277c82f
Ingo Hermsen <ingohermsen@gmail.com>
2022-12-16 17:45:21 +0100

    implemented JS Logic for choosing Category from dropdown (in Add Task)

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ let categories = [
   },
 ];
 
-// add Task form:
+// add Task:
 
 function addTask() {
-
+  
 };
+
 
 function categoryDropdown() {
     let dropdownElement = document.getElementById('categories');
@@ -32,28 +33,44 @@ function categoryDropdown() {
   	 dropdownElement.style.display = "block";
      dropdownElement.innerHTML = `<div><span onclick="setNewCategory()">new category</span></div>`;
   for(let i = 0; i < categories.length; i++) {
-  	 dropdownElement.innerHTML += `<div><span onclick="setCategory()">${categories[i]['name']}<span class="colorDot" style="background-color:${categories[i]['color']}"></span></div>`
+  	 dropdownElement.innerHTML += `<div><span onclick="setCategory(${i})">${createCategoryHtml(i)}</span></div>`
      };
     } else {
       dropdownElement.style.display = "none";
     };
 };
-  
+
+
+function setCategory(i) {
+    let categoryValue = document.getElementById('categoryValue');
+    categoryValue.innerHTML = `${createCategoryHtml(i)}`
+};
+
+ 
+function createCategoryHtml(i) {
+  return `${categories[i]['name']}<span class="colorDot" style="background-color:${categories[i]['color']}">`
+
+};
+
 
 function openDatePicker() {
 
 };
 
+
 function commitSubtasks() {
 
 };
+
 
 function clearInputFields() {
 
 };
 
+
 function setNewCategory() {
 
 };
+
 
 // 
