721045f07f4bf46b7f5e40c5327e869dde37475b
Rockadub <schroedermarkusmail@gmail.com>
2022-12-17 18:12:18 +0100

    Added Styles and functions

---
+++ js/contacts.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function editContact () {
+    document.querySelector(".new-contact-container").style.display = "flex";
+}
+
+function cancelContact () {
+    document.querySelector(".new-contact-container").style.display = "none";
+}
+
