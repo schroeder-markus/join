85aa15f4da511932d15a93e83a60612e758057ec
Rockadub <schroedermarkusmail@gmail.com>
2022-12-18 16:09:18 +0100

    added contacts js

--- js/contacts.js
+++ js/contacts.js
@@ -21,3 +21,21 @@ function closeEditContact() {
 function closeNewContact() {
     document.querySelector(".new-contact-container").style.display = "none";
 }
+
+// Add Contacts
+
+function addNewContact() {
+    let newContactData = {
+        "name": document.querySelector("#newContactSubmitName").value,
+        "mail": document.querySelector("#newContactSubmitEmail").value,
+        "phone": document.querySelector("#newContactSubmitPhone").value
+    }
+
+    contactData.push(newContactData)
+    document.querySelector("#newContactSubmitName").value = "";
+    document.querySelector("#newContactSubmitEmail").value = "";
+    document.querySelector("#newContactSubmitPhone").value = "";
+    closeNewContact();
+}
+
+
