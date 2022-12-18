62ae0c065fb45b347a90f84f08af8048e70d63fd
Rockadub <schroedermarkusmail@gmail.com>
2022-12-18 14:10:50 +0100

    adding js to contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,10 +1,21 @@
 "use strict"
 
 function editContact () {
-    document.querySelector(".new-contact-container").style.display = "flex";
+    document.querySelector(".edit-contact-container").style.display = "flex";
 }
 
 function cancelContact () {
-    document.querySelector(".new-contact-container").style.display = "none";
+    document.querySelector(".edit-contact-container").style.display = "none";
 }
 
+function createContact() {
+    document.querySelector(".new-contact-container").style.display = "flex";
+}
+
+function closeEditContact() {
+    document.querySelector(".edit-contact-container").style.display = "none";
+}
+
+function closeNewContact() {
+    document.querySelector(".new-contact-container").style.display = "none";
+}
