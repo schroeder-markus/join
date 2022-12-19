354d82b5e8bae6a9f9d92948aada026c47a9bbc7
Ingo Hermsen <ingohermsen@gmail.com>
2022-12-19 17:00:56 +0100

    made full javascript logic for new Categories (in Add Task > category Dropdown)

--- js/add_task.js
+++ js/add_task.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let selectedCategoryColor;
+let assignedContacts = [];
 
 function addTask() {
 
@@ -13,7 +14,8 @@ function categoryDropdown() {
 
     if (dropdownElement.style.display != "block") {
         dropdownElement.style.display = "block";
-        dropdownElement.innerHTML = `<div><span onclick="newEntryInput('categoryDropdown','newCategoryInput')">new category</span></div>`;
+        dropdownElement.innerHTML = `<div><span onclick="toggleView_DropdownAndNewEntry('categoryDropdown','newCategoryInput')">new category</span></div>`;      
+        renderColorSelection();
         for (let i = 0; i < categories.length; i++) {
             dropdownElement.innerHTML += `<div><span onclick="setCategory(${i})">${createCategoryHtml(i)}</span></div>`
         };
@@ -24,8 +26,8 @@ function categoryDropdown() {
 
 
 function setCategory(i) {
-    let categoryValue = document.getElementById('categoryValue');
-    categoryValue.innerHTML = `${createCategoryHtml(i)}`
+    let categoryInput = document.getElementById('categoryInput');
+    categoryInput.innerHTML = `${createCategoryHtml(i)}`
 };
 
 
@@ -35,17 +37,64 @@ function createCategoryHtml(i) {
 };
 
 
+function cancelInput() {
+    resetNewCategoryColorAndInput();
+    toggleView_DropdownAndNewEntry('newCategoryInput', 'categoryDropdown');
+};
+
+
+function setNewCategory() {
+    let categoryInputElement = document.querySelector('#newCategoryInput input');
+
+    if (selectedCategoryColor != undefined && categoryInputElement.value.length > 0) {
+        categories.push({
+            'name': `${categoryInputElement.value}`,
+            'color': `${selectedCategoryColor}`,
+        }
+        );
+        finishNewCategory(categoryInputElement.value);
+        resetNewCategoryColorAndInput();
+    } else {
+        showRequired()
+    };
+};
+
+
+function finishNewCategory(categoryName){
+    let categoryIndex = categories.findIndex(object => object.name === categoryName);
+    toggleView_DropdownAndNewEntry('newCategoryInput', 'categoryDropdown');
+    setCategory(categoryIndex);
+
+};
+
+function resetNewCategoryColorAndInput() {
+    let categoryInputElement = document.querySelector('#newCategoryInput input');
+    categoryInputElement.value = "";
+    selectedCategoryColor = undefined;
+}
+
+
+function showRequired() {
+    let colorSelAlertElement = document.getElementById('colorSelAlert');
+
+    if (selectedCategoryColor == undefined) {
+            colorSelAlertElement.style.display = "block";
+    }
+};
+
+
 function assignToDropdown() {
     let dropdownElement = document.getElementById('contactSelection');
     dropdownElement.innerHTML = "";
 
     if (dropdownElement.style.display != "block") {
         dropdownElement.style.display = "block";
-      
+        dropdownElement.innerHTML += `<div><span onclick="checkContact()">You<img id="contactCheckbox()" src="img/icon-selection-false.svg" alt="contactIconDark"</span></div>`;
+
         for (let i = 0; i < categories.length; i++) {
-            dropdownElement.innerHTML += `<div><span onclick="checkContact(${i})">firstName lastName${(i)}<img id="contactSelector(${i}" src="img/icon-selection-false.svg" alt="contactIconDark"</span></div>`
+            dropdownElement.innerHTML += `<div><span onclick="checkContact(${i})">firstName lastName${(i)}<img id="contactCheckbox(${i}" src="img/icon-selection-false.svg" alt="contactIconDark"</span></div>`
         };
-        dropdownElement.innerHTML += `<div><span onclick="newEntryInput('assignToDropdown','invitePersonInput')">Invite new Contact<img src="img/icon-contacts-dark.svg" alt="contactIconDark"</span></div>`;
+        dropdownElement.innerHTML += `<div><span onclick="toggleView_DropdownAndNewEntry('assignToDropdown','invitePersonInput')">Invite new Contact<img src="img/icon-contacts-dark.svg" alt="contactIconDark"</span></div>`;
     } else {
         dropdownElement.style.display = "none";
     };
@@ -57,32 +106,25 @@ function commitSubtask() {
 };
 
 
-function clearInputFields() {
+function toggleView_DropdownAndNewEntry(IDinvisibleElement, IDvisibleElement) {
+    let invisibleElement = document.getElementById(IDinvisibleElement);
+    let visibleElement = document.getElementById(IDvisibleElement);
 
-};
-
-
-function newEntryInput(dropdownID, inputID) {
-    let dropdownElement = document.getElementById(dropdownID);
-    let inputElement = document.getElementById(inputID);
-
-    dropdownElement.style.display = "none";
-    inputElement.style.display = "block";
-
-    renderColorSelection();
+    invisibleElement.style.display = "none";
+    visibleElement.style.display = "block";
 
 };
 
 
 function renderColorSelection() {
     let colorSelectionElement = document.getElementById('colorSelection');
-
     colorSelectionElement.innerHTML = '';
     for (let i = 0; i < categoryColors.length; i++) {
         colorSelectionElement.innerHTML += `
         <button onclick="setColor(${i})" type="button" id="dot${i}" class="colorDot"
-        style="background-color:${categoryColors[i]}" name="color"></button>`
-    };       
+        style="background-color:${categoryColors[i]}" name="color"></button>
+        `
+    };
 
 };
 
@@ -90,8 +132,10 @@ function renderColorSelection() {
 function setColor(i) {
     renderColorSelection();
     let colorDotElement = document.getElementById(`dot${i}`);
+    let colorSelAlertElement = document.getElementById('colorSelAlert');
     colorDotElement.classList.add('dotShadow');
     selectedCategoryColor = categoryColors[i];
+    colorSelAlertElement.style.display = "none";
 };
 
 
