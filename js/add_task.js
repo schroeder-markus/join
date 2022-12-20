871c3a3bc4fbc17a750f756bf2786288ba9e6fea
Ingo Hermsen <ingohermsen@gmail.com>
2022-12-20 18:08:11 +0100

    added first to addTask.js and set first contacts in assign-To dropdown

--- js/add_task.js
+++ js/add_task.js
@@ -3,13 +3,12 @@
 let selectedCategoryColor;
 let assignedContacts = [];
 let showCategoryList = false;
+let showContactList = false;
 
-function addTask() {
+// Toggles dropdown action (open / close) for the category Selection and the task assignment
+// Action Depends on the actual global boolean variable: showCategoryList / showContactList
 
-};
-
-
-function toggleDropdown() {
+function toggleCategoryDropdown() {
     showCategoryList = !showCategoryList;
 
     let dropdownContainer = document.getElementById('categoryDropdown');
@@ -22,6 +21,23 @@ function toggleDropdown() {
     };
 };
 
+
+function toggleAssignDropdown() {
+    showContactList = !showContactList;
+
+    let dropdownContainer = document.getElementById('assignToDropdown');
+
+    if (showContactList) {
+        renderContactSelection();
+        dropdownContainer.classList.add('showContactSelection');
+    } else {
+        dropdownContainer.classList.remove('showContactSelection');
+    };
+};
+
+// render functions for Dropdown Lists.
+// renders "New Category" plus categories from category-array with a for loop.
+
 function renderCategoryList() {
     let dropList = document.getElementById('categories');
     dropList.innerHTML = "";
@@ -32,28 +48,36 @@ function renderCategoryList() {
     renderColorSelection();
 };
 
+// creates an html element from given category Id (from categories array).
+// Then it returns the span element back to the original render function, including a span with the assigned category color as background-Color. 
 
 function createCategoryHtml(i) {
     return `${categories[i]['name']}<span class="colorDot" style="background-color:${categories[i]['color']}">`
 
 };
 
+// aborts New Category Dialogue
 
-
-function setCategory(i) {
-    let categoryInput = document.getElementById('categoryInput');
-    categoryInput.innerHTML = `${createCategoryHtml(i)}`
-};
-
-
-
-function cancelInput() {
+function cancelNewCategory() {
     resetNewCategoryColorAndInput();
     toggleView_DropdownAndNewEntry('newCategoryInput', 'categoryDropdown');
 };
 
+// resets selectable color for new Categories and empties the 'new Category' input field for further attempts.
+// fired after cancelling OR finishing new category.
 
-function setNewCategory() {
+function resetNewCategoryColorAndInput() {
+    let categoryInputElement = document.querySelector('#newCategoryInput input');
+    categoryInputElement.value = "";
+    selectedCategoryColor = undefined;
+};
+
+
+// fired after clicking the check-button in "new Category"-dialogue.
+// validates the input, what means if category name is filled and a color is selected.
+// success: finisches dialogue. fail: shows required-notification
+
+function confirmNewCategory() {
     let categoryInputElement = document.querySelector('#newCategoryInput input');
 
     if (selectedCategoryColor != undefined && categoryInputElement.value.length > 0) {
@@ -68,21 +92,23 @@ function setNewCategory() {
         showRequired()
     };
 };
-
+// finishes the 'New Category'-dialogue after a SUCCESSFUL validation check for color and Input Value (included in confirmNewCategory())
 
 function finishNewCategory(categoryName) {
     let categoryIndex = categories.findIndex(object => object.name === categoryName);
     toggleView_DropdownAndNewEntry('newCategoryInput', 'categoryDropdown');
-    setCategory(categoryIndex);
+    setCategorySelectionAsInput(categoryIndex);
 
 };
 
-function resetNewCategoryColorAndInput() {
-    let categoryInputElement = document.querySelector('#newCategoryInput input');
-    categoryInputElement.value = "";
-    selectedCategoryColor = undefined;
-}
+// after a new category is successfully validated, the chosen name and color are set as value in the original category field.
 
+function setCategorySelectionAsInput(i) {
+    let categoryInput = document.getElementById('categoryInput');
+    categoryInput.innerHTML = `${createCategoryHtml(i)}`
+};
+
+// after a failed validation in the "new category"-dialogue, the selected color the "required Alert" shows.
 
 function showRequired() {
     let colorSelAlertElement = document.getElementById('colorSelAlert');
@@ -92,6 +118,44 @@ function showRequired() {
     }
 };
 
+// renders Color Selection Buttons for new Categorys (visible after click on "New Category" in dropdown).
+
+function renderColorSelection() {
+    let colorSelectionElement = document.getElementById('colorSelection');
+    colorSelectionElement.innerHTML = '';
+    for (let i = 0; i < categoryColors.length; i++) {
+        colorSelectionElement.innerHTML += `
+        <button onclick="setColor(${i})" type="button" id="dot${i}" class="colorDot"
+        style="background-color:${categoryColors[i]}" name="color"></button>
+        `
+    };
+
+};
+
+// sets the chosen color for a new category as selected and then highlights the selection with a shadow class (dotShadow).
+
+function setColor(i) {
+    renderColorSelection();
+    let colorDotElement = document.getElementById(`dot${i}`);
+    let colorSelAlertElement = document.getElementById('colorSelAlert');
+    colorDotElement.classList.add('dotShadow');
+    selectedCategoryColor = categoryColors[i];
+    colorSelAlertElement.style.display = "none";
+};
+
+// renders actual User ("You") as selectable, all contacts from contact-array with a for loop and then adds the 'invite New Contact' option" .
+
+function renderContactSelection() {
+    let dropList = document.getElementById('contactSelection');
+    dropList.innerHTML = "";
+    dropList.innerHTML = `<div><span onclick="checkContact()">You<img id="contactCheckbox()" src="img/icon-selection-false.svg" alt="contactIconDark"</span></div>`
+    for (let i = 0; i < contactData.length; i++) {
+        dropList.innerHTML += `<div><span onclick="checkContact(${i})">${contactData[i].name}<img id="contactCheckbox(${i})" src="img/icon-selection-false.svg" alt="contactIconDarkEmpty"</span></div>`
+    };
+    dropList.InnerHTML += `<div><span onclick="toggleView_DropdownAndNewEntry('assignToDropdown','invitePersonInput')">Invite new Contact<img src="img/icon-contacts-dark.svg" alt="contactIconDark"</span></div>`;
+};
+
+// 
 
 function assignToDropdown() {
     let dropdownElement = document.getElementById('contactSelection');
@@ -101,8 +165,8 @@ function assignToDropdown() {
         dropdownElement.style.display = "block";
         dropdownElement.innerHTML += `<div><span onclick="checkContact()">You<img id="contactCheckbox()" src="img/icon-selection-false.svg" alt="contactIconDark"</span></div>`;
 
-        for (let i = 0; i < categories.length; i++) {
-            dropdownElement.innerHTML += `<div><span onclick="checkContact(${i})">firstName lastName${(i)}<img id="contactCheckbox(${i}" src="img/icon-selection-false.svg" alt="contactIconDark"</span></div>`
+        for (let i = 0; i < contactData.length; i++) {
+            dropdownElement.innerHTML += `<div><span onclick="checkContact(${i})">${contactData[i].name}<img id="contactCheckbox(${i})" src="img/icon-selection-false.svg" alt="contactIconDarkEmpty"</span></div>`
         };
         dropdownElement.innerHTML += `<div><span onclick="toggleView_DropdownAndNewEntry('assignToDropdown','invitePersonInput')">Invite new Contact<img src="img/icon-contacts-dark.svg" alt="contactIconDark"</span></div>`;
     } else {
@@ -125,30 +189,11 @@ function toggleView_DropdownAndNewEntry(IDinvisibleElement, IDvisibleElement) {
 
 };
 
-
-function renderColorSelection() {
-    let colorSelectionElement = document.getElementById('colorSelection');
-    colorSelectionElement.innerHTML = '';
-    for (let i = 0; i < categoryColors.length; i++) {
-        colorSelectionElement.innerHTML += `
-        <button onclick="setColor(${i})" type="button" id="dot${i}" class="colorDot"
-        style="background-color:${categoryColors[i]}" name="color"></button>
-        `
-    };
-
-};
-
-
-function setColor(i) {
-    renderColorSelection();
-    let colorDotElement = document.getElementById(`dot${i}`);
-    let colorSelAlertElement = document.getElementById('colorSelAlert');
-    colorDotElement.classList.add('dotShadow');
-    selectedCategoryColor = categoryColors[i];
-    colorSelAlertElement.style.display = "none";
-};
-
-
 function subtaskInput() {
     // u.a. muss hier das 'confirmOrcancel-Element' eingeblendet werden.
+};
+
+
+function addTask() {
+
 };
