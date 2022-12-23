f51dc6e5719989ce5af8833556cbaa12e3fe4133
Boubkir <109249622+Boubkir@users.noreply.github.com>
2022-12-23 09:49:15 +0100

    board responsive

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-'use strict'
+"use strict";
 
-setURL('https://gruppe-406.developerakademie.net/smallest_backend_ever');
+setURL("https://gruppe-406.developerakademie.net/smallest_backend_ever");
 
 let tasks = [];
 
 let categoryColors = [
-  'var(--label-1)',
-  'var(--label-2)',
-  'var(--label-3)',
-  'var(--label-5)',
-  'var(--label-6)',
-  'var(--label-7)',
+  "var(--label-1)",
+  "var(--label-2)",
+  "var(--label-3)",
+  "var(--label-5)",
+  "var(--label-6)",
+  "var(--label-7)",
 ];
 
 let categories = [
   {
-    'name': 'sales',
-    'color': 'var(--label-1)',
+    name: "sales",
+    color: "var(--label-1)",
   },
   {
-    'name': 'backoffice',
-    'color': 'var(--label-2)',
+    name: "backoffice",
+    color: "var(--label-2)",
   },
   {
-    'name': 'accounting',
-    'color': 'var(--label-3)',
+    name: "accounting",
+    color: "var(--label-3)",
   },
 ];
 
 function logOutClickAvatar() {
-  document.querySelector(".logout-btn").classList.toggle("toggle-logout-btn")
+  document.querySelector(".logout-btn").classList.toggle("toggle-logout-btn");
 }
