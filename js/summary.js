c43363cd190e67ab8cd9838c029b38f78ebb6f4d
Boubkir <109249622+Boubkir@users.noreply.github.com>
2022-12-30 15:06:55 +0100

    update

--- js/summary.js
+++ js/summary.js
@@ -3,7 +3,7 @@ let inProgress = allTasks.filter(t => t['status'] == 'inprogress');
 let awaitingFeedback = allTasks.filter(t => t['status'] == 'awaitingfeedback');
 let urgent = allTasks.filter(t => t['priority'] == 'urgent');
 let upcoming;
-let toDo = allTasks.filter(t => t['status'] == 'todo');
+
 let done = allTasks.filter(t => t['status'] == 'done');
 
 
@@ -19,11 +19,12 @@ async function loadTasks() {
     let x = new Date(a["Due Date"]) ;
     let y = new Date(b["Due Date"]);
     return x - y;
-  });
-  renderSummary();
+  })
+  let toDo = allTasks.filter(t => t['status'] == 'todo');
+  renderSummary(toDo);
 };
 
-function renderSummary() {
+function renderSummary(toDo) {
   document.getElementById('alltasks').innerHTML = allTasks.length;
   document.getElementById('tasksinprogress').innerHTML = inProgress.length;
   document.getElementById('awaitingfeedback').innerHTML = awaitingFeedback.length;
